979a8df9561507b8f64b79b1b29ff80240f9e2d8
Tech Alex Hub <131652274+techalexhub@users.noreply.github.com>
2025-04-27 22:51:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ API_HASH = os.environ.get("API_HASH", "")
 
 OWNER_ID = int(os.environ.get("OWNER_ID", ""))
 DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "VOLT2X)
+DB_NAME = os.environ.get("DB_NAME", "VOLT2X")
 
 
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
